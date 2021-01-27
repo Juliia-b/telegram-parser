cd4e0da6cf0fa81f2815179430509d1a49c53bfa
Julia Bezrukova <julika7391@gmail.com>
2021-01-27 14:34:07 +0500

    added tests for postgres

---
+++ db/postgres_test.go
@@ -0,0 +1,212 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/google/go-cmp/cmp"
+	"github.com/ory/dockertest/v3"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestMain(m *testing.M) {
+	var db *sql.DB
+	var err error
+	database := "postgres"
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.Run("postgres", "12.5", []string{"POSTGRES_PASSWORD=secret", "POSTGRES_DB=" + database})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err = pool.Retry(func() error {
+		var err error
+		testPort = resource.GetPort("5432/tcp")
+		db, err = sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", testPort, database))
+		if err != nil {
+			return err
+		}
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	code := m.Run()
+
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func TestPostgresClient_Insert(t *testing.T) {
+	pg, err := postgresTestConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// In the first cycle, all values must be entered into the table without errors
+	for _, testMsg := range testInsert {
+		err = pg.Insert(testMsg)
+		if err != nil {
+			t.Errorf(
+				"The first sycle.\nUnexpected error while entering %v into the database.\nERROR expected %v\nERROR got %v\n",
+				color.MagentaString("%#v", testMsg),
+				color.GreenString("<nil>"), color.RedString("%#v", err.Error()),
+			)
+		}
+	}
+
+	// The values of the second cycle should not be entered into the table, since it already contains rows with this set of testMsg ID and chat ID. An error must be returned
+	for _, testMsg := range testInsert {
+		err = pg.Insert(testMsg)
+		if err == nil {
+			t.Errorf(
+				"The second cycle. No error received while entering %v into the database.\nERROR expected %v\nERROR got %v\n",
+				color.MagentaString("%#v", testMsg),
+				color.GreenString("pq: duplicate key value violates unique constraint 'tg_parser_pkey'"),
+				color.RedString("<nil>"),
+			)
+		}
+	}
+
+	count, err := postgresTestGetRowsCount(pg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if count != len(testInsert) {
+		t.Errorf(
+			"Expected number of rows %v\ngot %v",
+			color.GreenString("%v", len(testInsert)), color.RedString("%v", count),
+		)
+	}
+}
+
+func TestPostgresClient_GetMessageById(t *testing.T) {
+	pg, err := postgresTestConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, test := range testGetMessageByIds {
+		message, err := pg.GetMessageById(test.ChatID, test.MessageID)
+
+		stringErr := fmt.Sprintf(
+			"For MessageID = %v and ChatID = %v\nMESSAGE expected %v\nMESSAGE got %v\nERROR expected %v\nERROR got %v\n",
+			color.MagentaString("%v", test.MessageID), color.MagentaString("%v", test.ChatID),
+			color.GreenString("%#v", test.ExpectedMessage), color.RedString("%#v", message),
+			color.GreenString("%#v", test.ErrorText), color.RedString("%#v", err),
+		)
+
+		if err != nil && !test.HasError {
+			t.Error(stringErr)
+			continue
+		}
+
+		if err == nil && test.ExpectedMessage == nil {
+			t.Error(stringErr)
+			continue
+		}
+
+		if !cmp.Equal(message, test.ExpectedMessage) {
+			t.Error(stringErr)
+			continue
+		}
+	}
+}
+
+func TestPostgresClient_Update(t *testing.T) {
+	pg, err := postgresTestConnection()
+	if err != nil {
+		log.Fatal("postgresTestConnection error: ", err)
+	}
+
+	for index, test := range testUpdates {
+
+		// Removing previous values in database
+		err = postgresTestDeleteRow(pg, test.InitMessage.ChatID, test.InitMessage.MessageID)
+		if err != nil {
+			log.Fatalf("RANGE INDEX %v\npostgresTestDeleteRow error: %v \n",
+				color.MagentaString("%v", index), color.RedString("%v", err.Error()))
+		}
+
+		var gotMessage *Message
+		var expectedMessage Message
+		var updateValues UpdateRow
+
+		gotMessage = test.InitMessage
+
+		expectedMessage = *test.InitMessage
+		updateValues = *test.UpdateRow
+
+		if test.ExpectedUpdateCount == 1 {
+			expectedMessage.ChatTitle = updateValues.NewChatTitle
+			expectedMessage.Content = updateValues.NewContent
+			expectedMessage.Views = updateValues.NewViews
+			expectedMessage.Forwards = updateValues.NewForwards
+			expectedMessage.Replies = updateValues.NewReplies
+		}
+
+		// MessageToUpdate is constant
+		err = pg.Insert(test.InitMessage)
+		if err != nil {
+			log.Fatalf(
+				"RANGE INDEX %v\nInsert error: %v\n",
+				color.MagentaString("%v", index), color.RedString("%v", err.Error()))
+		}
+
+		updateCount, err := pg.Update(test.UpdateRow)
+		if (err != nil && test.ExpectedUpdateCount == 1) || (updateCount != test.ExpectedUpdateCount) {
+
+			t.Error(fmt.Sprintf(
+				"RANGE INDEX %v\nInit message: %v\nUpdate values: %v\nRESULT expected %v\nRESULT got %v\nERROR expected %v\nERROR got %v\nCOUNT expected %v\nCOUNT got %v\n",
+				color.MagentaString("%#v", index),
+				color.MagentaString("%#v", test.InitMessage), color.MagentaString("%#v", test.UpdateRow),
+				color.GreenString("%#v", expectedMessage), color.RedString("%#v", gotMessage),
+				color.GreenString("%#v", test.ErrorText), color.RedString("%#v", err),
+				color.GreenString("%#v", test.ExpectedUpdateCount), color.RedString("%#v", updateCount),
+			))
+
+			continue
+		} else if err != nil && test.ExpectedUpdateCount == 0 {
+			continue
+		}
+
+		gotMessage, err = pg.GetMessageById(test.InitMessage.ChatID, test.InitMessage.MessageID)
+		if err != nil {
+			t.Errorf("RANGE INDEX %v\nUnexpected behavior. Error \"%v\" returned from method %v",
+				color.MagentaString("%#v", index),
+				color.RedString("%v", err.Error()), color.MagentaString("GetMessageById"))
+			continue
+		}
+
+		if !cmp.Equal(*gotMessage, expectedMessage) {
+			t.Error(fmt.Sprintf(
+				"RANGE INDEX %v\nInit message: %v\nUpdate values: %v\nRESULT expected %v\nRESULT got %v\nERROR expected %v\nERROR got %v\n",
+				color.MagentaString("%#v", index),
+				color.MagentaString("%#v", test.InitMessage), color.MagentaString("%#v", test.UpdateRow),
+				color.GreenString("%#v", expectedMessage), color.RedString("%#v", gotMessage),
+				color.GreenString("%#v", test.ErrorText), color.RedString("%#v", err),
+			))
+		}
+	}
+}
+
+//func TestPostgresClient_GetMessagesForATimePeriod(t *testing.T) {
+//	pg, err := postgresTestConnection()
+//	if err != nil {
+//		log.Fatal("postgresTestConnection error: ", err)
+//	}
+//
+//}
---
+++ db/postgres_test_helpers.go
@@ -0,0 +1,128 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+var testPort string
+
+//    --------------------------------------------------------------------------------
+//                                    STRUCTS
+//    --------------------------------------------------------------------------------
+
+//    ---------------------------------INSERT-----------------------------------------
+var (
+	// Insert
+	m1 = &Message{10, 1, "T1", "chat1-message10", 5000, 12, 13, 14}
+	m2 = &Message{11, 1, "T1", "chat1-message11", 7000, 40, 2, 1}
+	m3 = &Message{10, 5, "T5", "chat5-message10", 3000, 99, 50, 0}
+	m4 = &Message{11, 5, "T5", "chat5-message11", 7000, 20, 19, 14}
+	m5 = &Message{40, 9, "T9", "chat9-message40", 2000, 50, 20, 10}
+	m6 = &Message{41, 9, "T9", "chat9-message41", 3000, 40, 10, 0}
+)
+
+var testInsert = []*Message{m1, m2, m3, m4, m5, m6}
+
+//    --------------------------------GETMESSAGEBYID----------------------------------
+type testGetMessageById struct {
+	MessageID       int64
+	ChatID          int64
+	ExpectedMessage *Message
+	HasError        bool
+	ErrorText       string
+}
+
+var testGetMessageByIds = []*testGetMessageById{
+	// Messages with such IDs exist in the database. No error will be returned
+	{10, 1, m1, false, "<nil>"},
+	{10, 5, m3, false, "<nil>"},
+	{40, 9, m5, false, "<nil>"},
+
+	// Messages with such IDs do not exist in the database. An error will be returned
+	{10, 20, nil, true, "some error"},
+	{10, 40, nil, true, "some error"},
+	{17, 9, nil, true, "some error"},
+
+	//// Erroneous data. Should cause an error in the test
+	//{10, 1, m6, true, "nil"},
+	//{10, 5, nil, false, "nil"},
+	//{10, 20, nil, false, "not nil"},
+	//{10, 40, m6, true, "not nil"},
+}
+
+//    ------------------------------------UPDATE--------------------------------------
+var (
+	// To update values (after the test will be deleted from database)
+	m7 = &Message{20, 70, "OLD", "OLD-cont", 7000, 30, 20, 10}
+
+	// Will update data
+	u1 = &UpdateRow{20, 70, "OLD", "OLD-cont", 35, 25, 15}
+	u2 = &UpdateRow{20, 70, "NEW", "NEW-cont", 30, 20, 10}
+	u3 = &UpdateRow{20, 70, "NEW", "NEW-cont", 35, 25, 15}
+
+	// No such chat ID and message ID in database
+	u4 = &UpdateRow{1, 1, "OLD", "OLD-cont", 35, 25, 15}
+	u5 = &UpdateRow{2, 2, "NEW", "NEW-cont", 30, 20, 10}
+	u6 = &UpdateRow{3, 3, "NEW", "OLD-cont", 35, 25, 15}
+)
+
+type testUpdate struct {
+	InitMessage         *Message
+	UpdateRow           *UpdateRow
+	ErrorText           string
+	ExpectedUpdateCount int64
+}
+
+var testUpdates = []*testUpdate{
+	// The Update function will execute correctly. No error will be returned
+	{m7, u1, "<nil>", 1},
+	{m7, u2, "<nil>", 1},
+	{m7, u3, "<nil>", 1},
+
+	// There are no such rows in the database. An error will be returned
+	{m7, u4, "<nil>", 0},
+	{m7, u5, "<nil>", 0},
+	{m7, u6, "<nil>", 0},
+
+	// Erroneous data. Should cause an error in the test
+	//{m7, u4, "<nil>", 1},
+	//{m7, u1, "<nil>", 0},
+}
+
+//    ---------------------------GETMESSAGESFORATIMEPERIOD----------------------------
+// TODO add test
+
+//    --------------------------------------------------------------------------------
+//                                     HELPERS
+//    --------------------------------------------------------------------------------
+
+func postgresTestConnection() (*PostgresClient, error) {
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://postgres:secret@localhost:%s/%s?sslmode=disable", testPort, "postgres"))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS tg_parser ( message_id bigint, chat_id bigint, chat_title text, content text, date bigint, views integer, forwards integer, replies integer, PRIMARY KEY(message_id, chat_id) );`)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresClient{Connection: db, DbInfo: &DbInfo{"postgres", "tg_parser"}, SchemaInfo: getSchemaInfo(), TimePeriods: getTimePeriods()}, nil
+}
+
+func postgresTestGetRowsCount(pg *PostgresClient) (int, error) {
+	count := 0
+
+	row := pg.Connection.QueryRow("SELECT COUNT(*) FROM tg_parser")
+	err := row.Scan(&count)
+
+	return count, err
+}
+
+func postgresTestDeleteRow(pg *PostgresClient, chatId int64, messageId int64) error {
+	_, err := pg.Connection.Exec(fmt.Sprintf("DELETE FROM %v WHERE chat_id = %v AND message_id = %v;", pg.DbInfo.TableName, chatId, messageId))
+
+	return err
+}
