982a57cb4b90a9b56a375dc80213f2de5551d930
Julia Bezrukova <julika7391@gmail.com>
2021-01-27 20:19:35 +0500

    add configure logrus

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"github.com/Arman92/go-tdlib"
 	log "github.com/sirupsen/logrus"
 	"os"
-	"telegram-parser/app"
-	"telegram-parser/db"
+	"telegram-parser/flags"
 )
 
 func main() {
 	checkENV()
+	configureLogrus()
 
-	tdlib.SetLogVerbosityLevel(1)
-	tdlib.SetFilePath("./errors.txt")
+	//flags.ParseFlags()
+	flags.ParseFlags()
 
-	telegram := app.NewTgClient()
-	telegram.Authorization()
-
-	telegram.RunHandlingUpdates()
-
-	cli, _ := db.ConnectToPostgres()
-	//cli.GetAllData()
-	cli.GetMessageById(-1001129804770, 408447614976)
+	//tdlib.SetLogVerbosityLevel(1)
+	//tdlib.SetFilePath("./errors.txt")
+	//
+	//telegram := app.NewTgClient()
+	//telegram.Authorization()
+	//
+	//telegram.RunHandlingUpdates()
+	//
+	//cli, _ := db.ConnectToPostgres()
+	////cli.GetAllData()
+	//cli.GetMessageById(-1001129804770, 408447614976)
 
 	for {
 	}
@@ -46,25 +47,10 @@ func checkENV() {
 	}
 }
 
-func Log() {
-	filename := "logfile.log"
-	// Create the log file if doesn't exist. And append to it if it already exists.
-	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+func configureLogrus() {
 	Formatter := new(log.TextFormatter)
 	Formatter.TimestampFormat = "02-01-2006 15:04:05" // "Mon Jan 2 15:04:05 MST 2006"
 	Formatter.FullTimestamp = true
 
 	log.SetFormatter(Formatter)
-	if err != nil {
-		// Cannot open log file. Logging to stderr
-		fmt.Println(err)
-	} else {
-		log.SetOutput(f)
-	}
-
-	//log.Info("Some info. Earth is not flat")
-	//log.Warning("This is a warning")
-	//log.Error("Not fatal. An error. Won't stop execution")
-	//log.Fatal("MAYDAY MAYDAY MAYDAY")
-	//log.Panic("Do not panic")
 }
