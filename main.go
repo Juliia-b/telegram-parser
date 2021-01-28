a032628dd09bae8d6b9b28a6c65c4bd36e5c5e6c
Julia Bezrukova <julika7391@gmail.com>
2021-01-28 14:26:42 +0500

    configure logrus

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"github.com/banzaicloud/logrus-runtime-formatter"
+	"github.com/sirupsen/logrus"
 	"os"
-	"telegram-parser/flags"
 )
 
 func main() {
-	checkENV()
+
 	configureLogrus()
 
+	// ----------------------------
+	//checkENV()
+	//configureLogrus()
+	//
 	//flags.ParseFlags()
-	flags.ParseFlags()
 
 	//tdlib.SetLogVerbosityLevel(1)
 	//tdlib.SetFilePath("./errors.txt")
@@ -31,26 +34,34 @@ func main() {
 
 func checkENV() {
 	if os.Getenv("POSTGRESPASSWORD") == "" {
-		panic(`Environment variable "POSTGRESPASSWORD" is blank`)
+		logrus.Panic(`Environment variable "POSTGRESPASSWORD" is blank`)
 	}
 
 	if os.Getenv("TGTELEPHONENUMBER") == "" {
-		panic(`Environment variable "TGTELEPHONENUMBER" is blank`)
+		logrus.Panic(`Environment variable "TGTELEPHONENUMBER" is blank`)
 	}
 
 	if os.Getenv("TGAPIID") == "" {
-		panic(`Environment variable "TGAPIID" is blank`)
+		logrus.Panic(`Environment variable "TGAPIID" is blank`)
 	}
 
 	if os.Getenv("TGAPIHASH") == "" {
-		panic(`Environment variable "TGAPIHASH" is blank`)
+		logrus.Panic(`Environment variable "TGAPIHASH" is blank`)
 	}
 }
 
 func configureLogrus() {
-	Formatter := new(log.TextFormatter)
-	Formatter.TimestampFormat = "02-01-2006 15:04:05" // "Mon Jan 2 15:04:05 MST 2006"
-	Formatter.FullTimestamp = true
+	formatter := runtime.Formatter{
+		ChildFormatter: &logrus.TextFormatter{
+			TimestampFormat: "02-01-2006 15:04:05", // "Mon Jan 2 15:04:05 MST 2006"
+			FullTimestamp:   true,
+		},
+		File:         true,
+		BaseNameOnly: true}
 
-	log.SetFormatter(Formatter)
+	formatter.Line = true
+
+	logrus.SetFormatter(&formatter)
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
 }
