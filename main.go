190c99554d399c402c86d954f12823589ace601a
Julia Bezrukova <julika7391@gmail.com>
2021-02-03 18:08:17 +0500

    minor fixes

--- main.go
+++ main.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"github.com/Arman92/go-tdlib"
-	"github.com/banzaicloud/logrus-runtime-formatter"
 	"github.com/sirupsen/logrus"
-	"os"
 	"telegram-parser/app"
 	"telegram-parser/db"
 	"telegram-parser/flags"
 	"telegram-parser/grpc_client"
 	"telegram-parser/handling"
+	"telegram-parser/helpers"
 	"telegram-parser/mq"
 )
 
 func main() {
 	// System Setup
-	checkENV()
-	configureLogrus()
+	helpers.CheckENV()
+	helpers.ConfigureLogrus()
 
 	// инициализация канала для новых адресов нод
 	serviceAddresses := make(chan string)
@@ -36,7 +35,7 @@ func main() {
 	}
 
 	// структура общего назначения с доступом к базе, брокеру сообщений, подключения к клиентам сервиса и информации о состоянии консистентного хеша
-	handle := handling.NewHandleStruct(postgresClient, mq, serviceConnections)
+	handle := handling.NewHandleStruct(serviceConnections, postgresClient, mq)
 
 	// пытается подключиться ко всем адресам в списке
 	go handle.HandleNewServiceAddresses(serviceAddresses)
@@ -44,7 +43,7 @@ func main() {
 	flags.ParseFlags(serviceAddresses)
 
 	// запуск консьюмера очереди для получения сообщений и их обработки ( нахождение адреса ноды для отправки, отправка)
-	go handle.HandleMsgsFromMQ()
+	handle.RunHandlingMsgsFromMQ(20)
 
 	tdlib.SetLogVerbosityLevel(1)
 	tdlib.SetFilePath("./errors.txt")
@@ -54,62 +53,6 @@ func main() {
 
 	go telegramCli.GetUpdates(handle.Rabbit)
 
-	// ----------------------------------------
-	//
-	//
-	//
-	//
-	//
-	//
-	//conf := flags.ParseFlags()
-	//
-	//parserConn := grpc_client.NewParserConnections()
-	//parserConn.ParserClientsInit(conf.ParserAddrs)
-
-	//tdlib.SetLogVerbosityLevel(1)
-	//tdlib.SetFilePath("./errors.txt")
-	//
-	//telegram := app.NewTgClient()
-	//telegram.Authorization()
-	//
-	//telegram.RunHandlingUpdates()
-
-	//app.RedBlackTree()
-
 	forever := make(chan bool)
 	<-forever
-}
-
-func checkENV() {
-	if os.Getenv("POSTGRESPASSWORD") == "" {
-		logrus.Panic(`Environment variable "POSTGRESPASSWORD" is blank`)
-	}
-
-	if os.Getenv("TGTELEPHONENUMBER") == "" {
-		logrus.Panic(`Environment variable "TGTELEPHONENUMBER" is blank`)
-	}
-
-	if os.Getenv("TGAPIID") == "" {
-		logrus.Panic(`Environment variable "TGAPIID" is blank`)
-	}
-
-	if os.Getenv("TGAPIHASH") == "" {
-		logrus.Panic(`Environment variable "TGAPIHASH" is blank`)
-	}
-}
-
-func configureLogrus() {
-	formatter := runtime.Formatter{
-		ChildFormatter: &logrus.TextFormatter{
-			TimestampFormat: "02-01-2006 15:04:05", // "Mon Jan 2 15:04:05 MST 2006"
-			FullTimestamp:   true,
-		},
-		File:         true,
-		BaseNameOnly: true}
-
-	formatter.Line = true
-
-	logrus.SetFormatter(&formatter)
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(logrus.InfoLevel)
 }
